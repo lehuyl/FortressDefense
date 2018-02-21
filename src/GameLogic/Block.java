55e9f49906d694340bd6f28ee00c36390adff472
Steven <lehuyl@sfu.ca>
2018-02-21 00:30:09 -0800

    Refactor renamed Tetramino to Block

--- src/GameLogic/Block.java
+++ src/GameLogic/Block.java
@@ -1,24 +1,24 @@
 package GameLogic;
 
 /**
- * Class for the Tetramino block structure that makes up a Tank.
+ * Class for the Block structure that makes up a Tank.
  */
-public class Tetramino {
+public class Block {
     private boolean isAlive;
     private int row;
     private int column;
 
-    public Tetramino(int row, int column) {
+    public Block(int row, int column) {
         this.isAlive = true;
         this.row = row;
         this.column = column;
     }
 
     /**
-     * Alogrithm used to register a hit to Tetramino.
+     * Alogrithm used to register a hit to Block.
      * @param row Must not be null. Coordinate for row
      * @param col Must not be null. Coordinate for column
-     * @return Boolean containing whether or not this tetramino has been hit.
+     * @return Boolean containing whether or not this Block has been hit.
      */
     public boolean isHit(int row, int col)
     {
@@ -33,8 +33,8 @@ public class Tetramino {
     }
 
     /**
-     * Returns live status of Tetramino.
-     * @return  Boolean containing live status of Tetramino.
+     * Returns live status of Block.
+     * @return  Boolean containing live status of Block.
      */
     public boolean isAlive()
     {
@@ -42,7 +42,7 @@ public class Tetramino {
     }
 
     /**
-     * Set Tetramino to dead.
+     * Set Block to dead.
      * Call when hit returns true
      */
     private void setDead() {
